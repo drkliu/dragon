b101f717cc4dcc89581901823169fa8064f1f621
derek <liudrk@hotmail.com>
2020-11-22 19:59:37 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module dragon
+
+go 1.15
+
+require github.com/henrylee2cn/pholcus v1.3.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/henrylee2cn/pholcus/exec"
+)
+
+func main() {
+	fmt.Printf("aaa")
+	exec.DefaultRun("web")
+}
